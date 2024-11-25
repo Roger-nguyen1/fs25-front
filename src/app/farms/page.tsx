9109060bb96ec50f2c3197e19aa08d93c944ff37
Roger <roger.nguyen@hotmail.fr>
2024-11-25 22:16:37 +0100

    first commit : first page and farms page

---
+++ src/app/farms/page.tsx
@@ -0,0 +1,7 @@
+export default function Farms() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[url('/images/fs25-2.jpg')] bg-cover bg-center bg-no-repeat bg-opacity-0 animate-fade-in">
+      FARMS PAGE
+    </div>
+  );
+}
