c4ddb65f6986f007022607784774fc8b39d2f090
Roger-nguyen1 <roger.nguyen@hotmail.fr>
2024-12-05 11:24:40 +0100

    added loader spinner in farmdetails/id

--- src/app/farmdetails/[_id]/page.tsx
+++ src/app/farmdetails/[_id]/page.tsx
@@ -20,9 +20,11 @@ export default function FarmDetails() {
   const { _id } = useParams();
   const [farmData, setFarmData] = useState<Farm>();
   const [financesDatas, setFinancesDatas] = useState<any>();
+  const [isLoading, setIsloading] = useState<boolean>(false)
 
   useEffect(() => {
     const fetchFarms = async () => {
+      setIsloading(true)
       try {
         const response = await fetch(`/api/farms?_id=${_id}`, {
           cache: "no-store",
@@ -34,6 +36,8 @@ export default function FarmDetails() {
         setFinancesDatas(data.farm.finances[0]);
       } catch (error) {
         notFound();
+      } finally {
+        setIsloading(false)
       }
     };
     fetchFarms();
@@ -48,6 +52,16 @@ export default function FarmDetails() {
           <div className="flex items-center space-x-4">
             <Tractor className="w-12 h-12" />
             <CardTitle className="text-3xl">{farmData?.name}</CardTitle>
+            {isLoading && (
+            <div
+              className="inline-block h-9 w-9 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-white motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          )}
           </div>
         </CardHeader>
         <CardContent className="p-6">
